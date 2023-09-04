54ef231c68a57b709cca27a350621f5ef7bc0a32
DMcCallion <danielmccallion04@gmail.com>
2023-09-04 13:13:30 +0100

    RDS and RDS sg

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,3 +7,18 @@ variable "ACCESS_KEY_ID" {
   type = string
 
 }
+
+variable "DATABASE_NAME" {
+  type = string
+
+}
+
+variable "DATABASE_PASSWORD" {
+  type = string
+
+}
+
+variable "DATABASE_USERNAME" {
+  type = string
+
+}
