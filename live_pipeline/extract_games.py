25ec2a7e6477387491f8c05dec756a72cba9edac
gawsalya <rj_gawsalya@hotmail.co.uk>
2023-09-04 13:14:56 +0100

    extract complete

--- live_pipeline/extract_games.py
+++ live_pipeline/extract_games.py
@@ -11,7 +11,7 @@ def get_html(url):
     return html
 
 
-def parse_app_id_bs(html):
+def parse_app_id_bs(html) -> list[dict]:
     """function to find the app id from the url"""
 
     soup = BeautifulSoup(html, "html.parser")
@@ -21,13 +21,33 @@ def parse_app_id_bs(html):
     all_games = []
     for game in tags:
         application = {}
-        application['app_id'] = game.attrs['data-ds-appid']
-        application['title'] = game.find('span', class_='title').text
-        application['release_date'] = game.find(
+        application["app_id"] = game.attrs['data-ds-appid']
+        application["title"] = game.find('span', class_='title').text
+        application["release_date"] = game.find(
             'div', class_="col search_released responsive_secondrow").text
         all_games.append(application)
-        break
+
     return all_games
+
+
+def parse_game_bs(html) -> list[str]:
+    """function to find application's tags"""
+
+    soup = BeautifulSoup(html, "html.parser")
+    tags = soup.find_all("a", class_="app_tag")
+    game_tags = []
+    for each in tags:
+        game_tags.append(each.string.strip())
+
+    return game_tags
+
+
+def parse_price_bs(html):
+    """function to find application price"""
+
+    soup = BeautifulSoup(html, "html.parser")
+    tag = soup.find("div", class_="game_purchase_price price")
+    return tag.string.strip()
 
 
 if __name__ == "__main__":
@@ -35,6 +55,14 @@ if __name__ == "__main__":
     website = get_html(
         "https://store.steampowered.com/search/?sort_by=Released_DESC&category1=998&supportedlang=english&ndl=1")
     all_recent_games = parse_app_id_bs(website)
+    # print(all_recent_games)
 
     for game in all_recent_games:
-        game_webpage = get_html()
+        game_webpage = get_html(
+            f"https://store.steampowered.com/app/{game['app_id']}")
+        tags_for_game = parse_game_bs(game_webpage)
+        game["tags"] = tags_for_game
+        price_of_game = parse_price_bs(game_webpage)
+        game["price"] = price_of_game
+        print(game)
+        break
