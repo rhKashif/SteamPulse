1ea02d4f485145c37f8a83b2b50195cb76402667
DMcCallion <danielmccallion04@gmail.com>
2023-09-06 14:40:18 +0100

    link tables added

--- setup/schema.sql
+++ setup/schema.sql
@@ -1,4 +1,7 @@
 -- cleanup
+DROP TABLE IF EXISTS game_genre_link;
+DROP TABLE IF EXISTS game_developer_link;
+DROP TABLE IF EXISTS game_publisher_link;
 DROP TABLE IF EXISTS review;
 DROP TABLE IF EXISTS genre;
 DROP TABLE IF EXISTS developer;
@@ -6,7 +9,8 @@ DROP TABLE IF EXISTS publisher;
 DROP TABLE IF EXISTS game;
 DROP TABLE IF EXISTS platform;
 
--- tables with no foreign key
+
+-- tables with no foreign keys: platform, genre, publisher, developer
 
 
 CREATE TABLE platform(
@@ -18,6 +22,32 @@ CREATE TABLE platform(
 
 );
 
+
+CREATE TABLE genre(
+    genre_id SMALLINT GENERATED ALWAYS AS IDENTITY,
+    genre TEXT NOT NULL,
+    user_generated BOOLEAN NOT NULL,
+    PRIMARY KEY (genre_id),
+
+);
+
+
+CREATE TABLE developer(
+    developer_id SMALLINT GENERATED ALWAYS AS IDENTITY,
+    developer_name TEXT NOT NULL UNIQUE,
+    PRIMARY KEY (developer_id),
+    
+
+);
+
+
+CREATE TABLE publisher(
+    publisher_id SMALLINT GENERATED ALWAYS AS IDENTITY,
+    publisher_name TEXT NOT NULL UNIQUE,
+    PRIMARY KEY (publisher_id),
+
+
+);
 
 -- Game references platform
 
@@ -39,9 +69,7 @@ CREATE TABLE game(
 );
 
 
--- review, developer, publisher, and genre reference game
-
-
+-- review references game
 
 CREATE TABLE review(
     review_id INT GENERATED ALWAYS AS IDENTITY,
@@ -55,32 +83,38 @@ CREATE TABLE review(
 );
 
 
-CREATE TABLE genre(
-    genre_id SMALLINT GENERATED ALWAYS AS IDENTITY,
-    genre TEXT NOT NULL,
-    user_generated BOOLEAN NOT NULL,
-    game_id INT,
-    PRIMARY KEY (genre_id),
-    FOREIGN KEY (game_id) REFERENCES game(game_id)
-);
+-- Linking tables for game with developer / publisher / genre
 
 
-CREATE TABLE developer(
-    developer_id SMALLINT GENERATED ALWAYS AS IDENTITY,
-    developer_name TEXT NOT NULL UNIQUE,
-    game_id INT,
-    PRIMARY KEY (developer_id),
-    FOREIGN KEY (game_id) REFERENCES game(game_id)
+CREATE TABLE game_genre_link(
+    genre_link_id INT GENERATED ALWAYS AS IDENTITY,
+    game_id INT, 
+    genre_id SMALLINT,
+    PRIMARY KEY (genre_link_id),
+    FOREIGN KEY (game_id) REFERENCES game(game_id),
+    FOREIGN KEY (genre_id) REFERENCES genre(genre_id)
 
 );
 
 
-CREATE TABLE publisher(
-    publisher_id SMALLINT GENERATED ALWAYS AS IDENTITY,
-    publisher_name TEXT NOT NULL UNIQUE,
-    game_id INT,
-    PRIMARY KEY (publisher_id),
-    FOREIGN KEY (game_id) REFERENCES game(game_id)
+CREATE TABLE game_developer_link(
+    developer_link_id INT GENERATED ALWAYS AS IDENTITY,
+    game_id INT, 
+    developer_id SMALLINT,
+    PRIMARY KEY (developer_link_id),
+    FOREIGN KEY (game_id) REFERENCES game(game_id),
+    FOREIGN KEY (developer_id) REFERENCES developer(developer_id)
+
+);
+
+
+CREATE TABLE game_publisher_link(
+    publisher_link_id INT GENERATED ALWAYS AS IDENTITY,
+    game_id INT, 
+    publisher_id SMALLINT,
+    PRIMARY KEY (publisher_link_id),
+    FOREIGN KEY (game_id) REFERENCES game(game_id),
+    FOREIGN KEY (publisher_id) REFERENCES publisher(publisher_id)
 
 );
 
@@ -101,5 +135,5 @@ VALUES
     (TRUE, TRUE, FALSE);
     
 
--- Hard to seed games / devs / publishers / genre
+-- Hard to seed games / developers / publishers / genre
 -- although we could pick 5 very popular games to manually enter and test our review loading against
