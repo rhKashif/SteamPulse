2c60b32b620d48d23c266696c85161cdc90d1d2d
angelikavilde <angelikavilde@gmail.com>
2023-09-06 11:02:43 +0100

    added a function to remove any rows where cell values are empty:

--- pipeline_reviews/transform.py
+++ pipeline_reviews/transform.py
@@ -32,7 +32,7 @@ def get_release_date(game_id: int, conn: connection, cache: dict) -> datetime:
 
 
 def correct_playtime(reviews_df: DataFrame) -> DataFrame:
-    """Returns a datatframe with valid playtime recordings only"""
+    """Returns a data-frame with valid playtime recordings only"""
     release_date_cache = {}
     conn = get_db_connection()
     reviews_df["release_date"] = reviews_df["game_id"].apply(lambda row: get_release_date(
@@ -44,8 +44,26 @@ def correct_playtime(reviews_df: DataFrame) -> DataFrame:
         reviews_df["playtime_at_review"] < reviews_df["maximum_playtime_since_release"]]
     reviews_df["full_playtime"] = reviews_df[
         reviews_df["full_playtime"] < reviews_df["maximum_playtime_since_release"]]
+    reviews_df.drop(columns=["maximum_playtime_since_release","release_date"], inplace=False)
+    return reviews_df
+
+
+def remove_empty_rows(reviews_df: DataFrame) -> DataFrame:
+    """Returns a data-frame without empty values in columns"""
+    reviews_df = reviews_df[reviews_df["game_id"].notna() & reviews_df["review"].notna()
+        & reviews_df["review_score"].notna() & reviews_df["playtime_at_review"].notna()
+        & reviews_df["full_playtime"].notna()]
+    return reviews_df
+
+
+def change_column_types(reviews_df: DataFrame) -> DataFrame:
+    """Returns a data-frame with correct data types"""
+    # TODO
 
 
 if __name__ == "__main__":
+
     reviews = pd.read_csv("reviews.csv")
+    reviews = remove_empty_rows(reviews)
+    reviews = change_column_types(reviews)
     reviews = correct_playtime(reviews)
