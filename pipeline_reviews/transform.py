2de0adc19861513d8ccfa7f389846d50f0f84e21
angelikavilde <angelikavilde@gmail.com>
2023-09-06 13:30:29 +0100

    added suggestions from pull request and error types

--- pipeline_reviews/transform.py
+++ pipeline_reviews/transform.py
@@ -3,12 +3,12 @@
 from datetime import datetime
 from os import environ
 
+from dotenv import load_dotenv
 import pandas as pd
 from pandas import DataFrame
-from dotenv import load_dotenv
-from psycopg2.extras import RealDictCursor
-from psycopg2 import connect
+from psycopg2 import connect, Error
 from psycopg2.extensions import connection
+from psycopg2.extras import RealDictCursor
 
 
 def get_db_connection() -> connection:
@@ -32,27 +32,28 @@ def get_release_date(game_id: int, conn: connection, cache: dict) -> datetime:
 
 def correct_playtime(reviews_df: DataFrame) -> DataFrame:
     """Returns a data-frame with valid playtime recordings only"""
-    release_date_cache = {}
-    # TODO conn = get_db_connection()
-    conn = 5
-    reviews_df["release_date"] = reviews_df["game_id"].apply(lambda row: get_release_date(
-        row, conn, release_date_cache))
-    time_now = datetime.now()
-    reviews_df["maximum_playtime_since_release"] = reviews_df["release_date"].apply(
-        lambda row: (time_now - row).total_seconds()/3600)
-    reviews_df = reviews_df[
-        reviews_df["playtime_at_review"] <= reviews_df["maximum_playtime_since_release"]]
-    reviews_df = reviews_df[
-        reviews_df["full_playtime"] <= reviews_df["maximum_playtime_since_release"]]
-    reviews_df.drop(columns=["maximum_playtime_since_release","release_date"], inplace=True)
+    try:
+        release_date_cache = {}
+        # TODO conn = get_db_connection()
+        conn = 5
+        reviews_df["release_date"] = reviews_df["game_id"].apply(lambda row: get_release_date(
+            row, conn, release_date_cache))
+        time_now = datetime.now()
+        reviews_df["maximum_playtime_since_release"] = reviews_df["release_date"].apply(
+            lambda row: (time_now - row).total_seconds()/3600)
+        reviews_df = reviews_df[
+            reviews_df["playtime_at_review"] <= reviews_df["maximum_playtime_since_release"]]
+        reviews_df = reviews_df[
+            reviews_df["full_playtime"] <= reviews_df["maximum_playtime_since_release"]]
+        reviews_df.drop(columns=["maximum_playtime_since_release","release_date"], inplace=True)
+    except Error as e:
+        print("Connection Error: ", e)
     return reviews_df
 
 
 def remove_empty_rows(reviews_df: DataFrame) -> DataFrame:
     """Returns a data-frame without empty values in columns"""
-    reviews_df = reviews_df[reviews_df["game_id"].notna() & reviews_df["review"].notna()
-        & reviews_df["review_score"].notna() & reviews_df["playtime_at_review"].notna()
-        & reviews_df["full_playtime"].notna()]
+    reviews_df.dropna(axis=1, how="any", inplace=True)
     return reviews_df
 
 
@@ -63,9 +64,16 @@ def change_column_types(reviews_df: DataFrame) -> DataFrame:
     for column in columns_to_numeric:
         reviews_df[column] = pd.to_numeric(reviews_df[column], errors="coerce")
         reviews_df = reviews_df.dropna(subset=[column])
-    reviews_df["last_timestamp"] = reviews_df["last_timestamp"].apply(lambda row:
-                    datetime.strptime(row, "%Y-%m-%d %H:%M:%S"))
+    reviews_df["last_timestamp"] = reviews_df["last_timestamp"].apply(validate_time_string)
     return reviews_df
+
+
+def validate_time_string(row: str) -> datetime | None:
+    """Returns datetime object or None if wrong format"""
+    try:
+        return datetime.strptime(row, "%Y-%m-%d %H:%M:%S")
+    except (TypeError, ValueError):
+        return None
 
 
 def correct_cell_values(reviews_df: DataFrame) -> DataFrame:
@@ -90,8 +98,8 @@ def remove_duplicate_reviews(review_df: DataFrame) -> DataFrame:
 if __name__ == "__main__":
 
     reviews = pd.read_csv("reviews.csv")
-    reviews = remove_empty_rows(reviews)
     reviews = change_column_types(reviews)
+    reviews = remove_empty_rows(reviews)
     reviews = correct_cell_values(reviews)
     reviews = remove_duplicate_reviews(reviews)
     reviews = correct_playtime(reviews)
