26cbbc6c2e82b4e65b0bc9795d43317c94135b33
gawsalya <rj_gawsalya@hotmail.co.uk>
2023-09-07 11:52:25 +0100

    function for adding genre created

--- pipeline_games/load_games.py
+++ pipeline_games/load_games.py
@@ -24,17 +24,57 @@ def execute_batch_columns(conn, data_frame: pd.DataFrame, table: str, column: st
     tuples = tuples = list(zip(data_frame.unique()))
     cols = column
     query = "INSERT INTO %s(%s) VALUES(%%s)" % (table, cols)
-    cursor = conn.cursor()
+    with conn.cursor(cursor_factory=RealDictCursor) as cur:
+        try:
+            execute_batch(cur, query, tuples, page_size)
+            conn.commit()
+        except (Exception, DatabaseError) as error:
+            print("Error: %s" % error)
+            conn.rollback()
+            cur.close()
+            return 1
+        print("execute_batch() done")
+        cur.close()
+
+
+def execute_batch_columns_for_genres(conn, data_frame: pd.DataFrame, table: str, page_size=100):
+    """Using psycopg2.extras.execute_batch() to insert the genres into database"""
+    tuples = [tuple(x) for x in data_frame.to_numpy()]
+    cols = ','.join(list(data_frame.columns))
+
+    query = "INSERT INTO %s(%s) VALUES(%%s,%%s)" % (table, cols)
+    with conn.cursor(cursor_factory=RealDictCursor) as cur:
+        try:
+            execute_batch(cur, query, tuples, page_size)
+            conn.commit()
+        except (Exception, DatabaseError) as error:
+            print("Error: %s" % error)
+            conn.rollback()
+            cur.close()
+            return 1
+        print("execute_batch() done")
+        cur.close()
+
+
+def get_existing_data(variable_name: str, table_name: str, value_name: str, value: str, conn: connect, cache: dict):
+    """Retrieves the existing data and adds to a cache dict given a provided value and table"""
     try:
-        execute_batch(cursor, query, tuples, page_size)
-        conn.commit()
-    except (Exception, DatabaseError) as error:
-        print("Error: %s" % error)
-        conn.rollback()
-        cursor.close()
-        return 1
-    print("execute_batch() done")
-    cursor.close()
+        with conn.cursor(cursor_factory=RealDictCursor) as cur:
+            cur.execute(
+                sql.SQL("""SELECT {variable_name} FROM {table_name} WHERE {value_name} = %s;""").format(
+                    variable_name=sql.Identifier(variable_name), table_name=sql.Identifier(table_name),
+                    value_name=sql.Identifier(value_name)), [value])
+            id_value = cur.fetchone()[variable_name]
+            cache[value] = id_value
+            cur.close()
+    except:
+        return None
+
+
+def check_if_in_cache(name: str, cache: dict):
+    if name in cache.keys():
+        return None
+    return name
 
 
 def add_game_information(conn, data: list):
@@ -75,26 +115,6 @@ def add_genre_information(conn, data: list):
         cur.close()
 
 
-def get_existing_data(variable_name: str, table_name: str, value_name: str, value: str, conn: connect, cache: dict):
-    """Retrieves the existing data and adds to a cache dict given a provided value and table"""
-    try:
-        with conn.cursor(cursor_factory=RealDictCursor) as cur:
-            cur.execute(
-                sql.SQL("""SELECT {variable_name} FROM {table_name} WHERE {value_name} = %s;""").format(
-                    variable_name=sql.Identifier(variable_name), table_name=sql.Identifier(table_name),
-                    value_name=sql.Identifier(value_name)), [value])
-            id_value = cur.fetchone()[variable_name]
-            cache[value] = id_value
-    except:
-        return None
-
-
-def check_if_in_cache(name: str, cache: dict):
-    if name in cache.keys():
-        return None
-    return name
-
-
 if __name__ == "__main__":
     load_dotenv()
     configuration = environ
@@ -110,5 +130,27 @@ if __name__ == "__main__":
 
     execute_batch_columns(connection, new_developers,
                           'developer', 'developer_name', page_size=100)
+
+    publishers_cache = {}
+    data_frame["publishers"].apply(lambda row: get_existing_data("publisher_id", "publisher", "publisher_name",
+                                                                                 row, connection, publishers_cache))
+    new_publishers = data_frame["publishers"].apply(
+        lambda row: check_if_in_cache(row, publishers_cache)).dropna(axis=0)
+
+    execute_batch_columns(connection, new_publishers,
+                          'publisher', 'publisher_name', page_size=100)
+
+    genres_cache = {}
+    data_frame["genre"].apply(lambda row: get_existing_data("genre_id", "genre", "genre",
+                                                            row, connection, genres_cache))
+
+    genres = data_frame.copy()
+    genres['genre'] = genres["genre"].apply(
+        lambda row: check_if_in_cache(row, genres_cache)).dropna(axis=0)
+
+    genres = genres[['genre', 'user_generated']].drop_duplicates()
+
+    execute_batch_columns_for_genres(connection, genres,
+                                     'genre', page_size=100)
 
     # execute_batch_columns(connection, data_frame,'publisher', 'publisher_name', page_size=100)
