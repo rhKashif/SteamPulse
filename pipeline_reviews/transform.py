275e8f72fff2dd66b0f8f3eb6e76c1a61b6285e0
angelikavilde <angelikavilde@gmail.com>
2023-09-07 13:18:40 +0100

    removed column full_playtime

--- pipeline_reviews/transform.py
+++ pipeline_reviews/transform.py
@@ -35,8 +35,6 @@ def correct_playtime(reviews_df: DataFrame) -> DataFrame:
             lambda row: (time_now - row).total_seconds()/3600)
         reviews_df = reviews_df[
             reviews_df["playtime_at_review"] <= reviews_df["maximum_playtime_since_release"]]
-        reviews_df = reviews_df[
-            reviews_df["full_playtime"] <= reviews_df["maximum_playtime_since_release"]]
         reviews_df.drop(columns=["maximum_playtime_since_release","release_date"], inplace=True)
     except Error as e:
         print("Connection Error: ", e)
@@ -51,7 +49,7 @@ def remove_empty_rows(reviews_df: DataFrame) -> DataFrame:
 
 def change_column_types(reviews_df: DataFrame) -> DataFrame:
     """Returns a data-frame with correct data types"""
-    columns_to_numeric = ["review_score","playtime_at_review","full_playtime"]
+    columns_to_numeric = ["review_score","playtime_at_review"]
 
     for column in columns_to_numeric:
         reviews_df[column] = pd.to_numeric(reviews_df[column], errors="coerce")
@@ -71,20 +69,16 @@ def validate_time_string(row: str) -> datetime | None:
 
 def correct_cell_values(reviews_df: DataFrame) -> DataFrame:
     """Drops rows with invalid cell values"""
-    columns = ["review_score", "playtime_at_review", "full_playtime"]
-
-    for column in columns:
-        reviews_df = reviews_df[reviews_df[column] >= 0]
+    reviews_df = reviews_df[reviews_df["review_score"] >= 0]
     # for reviews from players who did play the game
-    reviews_df = reviews_df[reviews_df["playtime_at_review"] > 1]
-    reviews_df = reviews_df[reviews_df["playtime_at_review"] <= reviews_df["full_playtime"]]
+    reviews_df = reviews_df[reviews_df["playtime_at_review"] >= 1]
     return reviews_df
 
 
 def remove_duplicate_reviews(review_df: DataFrame) -> DataFrame:
     """Removes duplicate rows"""
     review_df.drop_duplicates(
-        subset=["review","game_id","playtime_at_review","full_playtime"], inplace=True)
+        subset=["review","game_id","playtime_at_review"], inplace=True)
     return review_df
 
 
@@ -95,7 +89,7 @@ if __name__ == "__main__":
     reviews = remove_empty_rows(reviews)
     reviews = correct_cell_values(reviews)
     reviews = remove_duplicate_reviews(reviews)
-    reviews = correct_playtime(reviews)
+    # reviews = correct_playtime(reviews)
     if "Unnamed: 0" in reviews.columns:
         reviews.drop(columns="Unnamed: 0", inplace=True)
     reviews.to_csv("reviews.csv") #! index=False potentially for load
