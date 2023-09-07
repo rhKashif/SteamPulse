592e846d14aa043a9ef3d40a3049247d90653448
gawsalya <rj_gawsalya@hotmail.co.uk>
2023-09-07 11:00:22 +0100

    cache function finished! batch processing works

--- pipeline_games/load_games.py
+++ pipeline_games/load_games.py
@@ -2,7 +2,7 @@
 from os import environ
 from dotenv import load_dotenv
 import pandas as pd
-from psycopg2 import connect, DatabaseError
+from psycopg2 import connect, DatabaseError, sql
 from psycopg2.extras import RealDictCursor, execute_batch
 
 
@@ -21,7 +21,7 @@ def get_db_connection(config):
 
 def execute_batch_columns(conn, data_frame: pd.DataFrame, table: str, column: str, page_size=100):
     """Using psycopg2.extras.execute_batch() to insert the dataframe"""
-    tuples = list(zip(data_frame['developers'].unique()))
+    tuples = tuples = list(zip(data_frame.unique()))
     cols = column
     query = "INSERT INTO %s(%s) VALUES(%%s)" % (table, cols)
     cursor = conn.cursor()
@@ -75,20 +75,24 @@ def add_genre_information(conn, data: list):
         cur.close()
 
 
-def get_existing_data(table_name: str, value: str, conn: connect, cache: dict):
-    """Retrieves the existing data for a game with a provided value and table"""
-    if value in cache.keys():
-        return cache[value]
-    else:
+def get_existing_data(variable_name: str, table_name: str, value_name: str, value: str, conn: connect, cache: dict):
+    """Retrieves the existing data and adds to a cache dict given a provided value and table"""
+    try:
         with conn.cursor(cursor_factory=RealDictCursor) as cur:
-        try: 
             cur.execute(
-                """SELECT developer_id FROM developer WHERE developer_name = %s;""", [value])
-            id_value = cur.fetchone()['developer_id']
-        cache[value] = id_value
-        except:
-            ## add in the value if it does not exist and then return the cache
-        return cache[value]
+                sql.SQL("""SELECT {variable_name} FROM {table_name} WHERE {value_name} = %s;""").format(
+                    variable_name=sql.Identifier(variable_name), table_name=sql.Identifier(table_name),
+                    value_name=sql.Identifier(value_name)), [value])
+            id_value = cur.fetchone()[variable_name]
+            cache[value] = id_value
+    except:
+        return None
+
+
+def check_if_in_cache(name: str, cache: dict):
+    if name in cache.keys():
+        return None
+    return name
 
 
 if __name__ == "__main__":
@@ -99,11 +103,12 @@ if __name__ == "__main__":
     data_frame = pd.read_csv("genres.csv")
 
     developers_cache = {}
-    data_frame["developers"] = data_frame["developers"].apply(lambda row: get_release_date("developers",
-                                                                                           row, connection, developers_cache))
+    data_frame["developers"].apply(lambda row: get_existing_data("developer_id", "developer", "developer_name",
+                                                                                 row, connection, developers_cache))
+    new_developers = data_frame["developers"].apply(
+        lambda row: check_if_in_cache(row, developers_cache)).dropna(axis=0)
 
-    print(developers_cache)
-
-    # execute_batch_columns(connection, data_frame,'developer', 'developer_name', page_size=100)
+    execute_batch_columns(connection, new_developers,
+                          'developer', 'developer_name', page_size=100)
 
     # execute_batch_columns(connection, data_frame,'publisher', 'publisher_name', page_size=100)
