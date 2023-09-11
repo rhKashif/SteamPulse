7052cbdd2786bc3c7b94d27b643c198e3588878c
angelikavilde <angelikavilde@gmail.com>
2023-09-11 15:43:02 +0100

    finished tests for transform

--- pipeline_reviews/test_transform.py
+++ pipeline_reviews/test_transform.py
@@ -3,10 +3,12 @@
 from datetime import date
 
 from pandas import DataFrame
+from numpy import int64
 from unittest.mock import MagicMock
 
 from transform import get_release_date, remove_empty_rows, validate_time_string
-from transform import remove_duplicate_reviews, remove_unnamed
+from transform import remove_duplicate_reviews, remove_unnamed, correct_cell_values
+from transform import change_column_types, correct_playtime
 
 
 def test_get_release_date():
@@ -23,11 +25,10 @@ def test_remove_empty_rows():
     assert remove_empty_rows(fake_df).empty
 
 
-def test_validate_time_string_basic():
+def test_validate_time_string_basic(time_string):
     """Verifies that correct object type
     is returned (date) from a timestamp string"""
-    timestamp_string = "2019-02-23 12:13:10"
-    assert isinstance(validate_time_string(timestamp_string), date)
+    assert isinstance(validate_time_string(time_string), date)
 
 
 def test_validate_time_string_error():
@@ -46,3 +47,25 @@ def test_remove_unnamed():
     """Verifies that unnamed column is removed if exists"""
     fake_df = DataFrame([{"Unnamed: 0": 1, "test": "test2"}])
     assert remove_unnamed(fake_df).shape == (1,1)
+
+
+def test_correct_cell_values(fake_df_transform):
+    """Verifies that rows are removed with incorrect cell values"""
+    assert correct_cell_values(fake_df_transform).shape == (1,4)
+
+
+def test_change_column_types(fake_df_transform):
+    """Verifies that column types are correctly changed"""
+    returned_df = change_column_types(fake_df_transform)
+    assert all(isinstance(val, date) for val in returned_df["last_timestamp"].values)
+    assert all(isinstance(val, int64) for val in returned_df["review_score"].values)
+    assert all(isinstance(val, int64) for val in 
+               returned_df["playtime_last_2_weeks"].values)
+
+
+def test_correct_playtime(monkeypatch, time_string, fake_df_transform):
+    """Verifies that function correctly identifies that playtime is valid"""
+    monkeypatch.setattr("transform.get_db_connection", lambda *args: None)
+    monkeypatch.setattr("transform.get_release_date", 
+                    lambda *args: validate_time_string(time_string))
+    assert correct_playtime(fake_df_transform).equals(fake_df_transform)
