df9184d7cf69e17cbe71d246146a81e48ec2ac02
angelikavilde <angelikavilde@gmail.com>
2023-09-11 14:27:56 +0100

    added 2 fixtures for testing sentiment and extract

--- pipeline_reviews/conftest.py
+++ pipeline_reviews/conftest.py
@@ -3,8 +3,21 @@
 from pytest import fixture
 from pandas import DataFrame
 
+
 @fixture
-def fake_df():
+def fake_df_load():
     """Fake data-frame used for testing"""
     return DataFrame([{"game_id": 2, "test": 9},
         {"game_id": 3, "test": 5}, {"game_id": 8, "test": None}], index=[1,2,3])
+
+
+@fixture
+def fake_review() -> str:
+    """Returns a fake review for testing"""
+    return "Test\n,;review fail"
+
+
+@fixture
+def fake_df_sentiment(fake_review) -> DataFrame:
+    """Returns data-frame used for testing"""
+    return DataFrame([{"review": fake_review}], index=[1])
